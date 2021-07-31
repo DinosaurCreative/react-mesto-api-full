a4603076336ac99e50fe127db55cf181f9c14e89
German <gimalaevgerman@yandex.ru>
2021-07-31 13:25:44 +0300

    feat: Добавь базу юзеров и упраление ей

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
-// const BASE_PATH = "mongodb://localhost:27017/mestodb";
-const { PORT = 3000 ,BASE_PATH } = process.env;
 
+const { PORT = 3000 } = process.env;
 const app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+
+// app.use(express.static(path.resolve(__dirname)));
+app.use('/users', require('./routes/users'));
+
 app.listen(PORT, () => {
-  console.log(`Ссылка на сервер: ${PORT}`);
+  console.log(`Работаем на : ${PORT}`);
 });
