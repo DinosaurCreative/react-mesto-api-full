c1e1a57fd15e25bd4aae80880eb539bb0c914d11
German <gimalaevgerman@yandex.ru>
2021-08-28 20:35:10 +0300

    Поправил app.js для удачного мерджа

--- backend/app.js
+++ backend/app.js
@@ -12,18 +12,15 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://praktikum.tk');
-  next();
-});
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
 
 app.post('/signup', createUserValidation, createUser);
