447626997819a01564a5c2e14d26b6466f0333b6
German <gimalaevgerman@yandex.ru>
2021-09-01 22:40:30 +0300

    Поправил модуль корс

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const corsHandler = require('./middlewares/cors');
+// const corsHandler = require('./middlewares/cors');
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -21,7 +22,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-app.use(corsHandler);
+
+app.use(cors());
+// app.use(corsHandler);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
