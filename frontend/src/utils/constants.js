8b463055a6a7ca8453c457245ab260335f852b62
German <gimalaevgerman@yandex.ru>
2022-03-28 13:47:09 +0300

    fix: change api adress link

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://api.lookaround.nomoredomains.club";
+// export const BASE_URL = "https://api.lookaround.nomoredomains.club";
+export const BASE_URL = "https://react-mesto-api-full-self.vercel.app";
