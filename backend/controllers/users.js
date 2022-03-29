44c28d085524e651215ec4db010d9568fd402808
German <gimalaevgerman@yandex.ru>
2022-03-29 16:52:31 +0300

    change JWT_SECRET from str to const

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -122,7 +122,7 @@ module.exports.login = (req, res, next) => {
 
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? 'JWT_SECRET' : 'super-strong-secret', { expiresIn: '7d' });
       res.cookie('_id', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
   const token = extractBearerToken(authorization);
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? 'JWT_SECRET' : 'super-strong-secret');
   } catch (err) {
     next(new UnauthorizedError('Ошибка авторизации'));
   }
