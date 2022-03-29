b054feccbffa91f70c3cf9d2775e7745802ba0c7
German <gimalaevgerman@yandex.ru>
2022-03-29 17:35:11 +0300

    change back jwt secret key

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -7,7 +7,6 @@ const NotFoundError = require('../errors/NotFoundError');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 const ConflictError = require('../errors/ConflictError');
 
-// const { NODE_ENV } = process.env;
 const { JWT_SECRET, NODE_ENV } = process.env;
 const {
   usersIdMissing,
@@ -122,7 +121,7 @@ module.exports.login = (req, res, next) => {
 
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? 'JWT_SECRET' : 'super-strong-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret', { expiresIn: '7d' });
       res.cookie('_id', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
